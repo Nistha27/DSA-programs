7e42fa1084a49410b0aef90072addf87ecf2bde3
Nistha <codernistha27@gmail.com>
2024-01-28 11:35:28 +0530

    circular linked list added

--- ssl_operations.c
+++ ssl_operations.c
@@ -4,7 +4,7 @@ struct node{
     struct node *link;
 };
 typedef struct node *NODE;
-NODE first = NULL;
+NODE last = NULL;
 NODE create()
 {
     NODE snode;
@@ -23,46 +23,71 @@ NODE insert_front()
 {
     NODE temp;
     temp = create();
-    if(first == NULL)
+    if(last == NULL)
     {
         return temp;
     }
-    if(first->link=NULL)
+    if(last->link=last)
     {
-        temp->link = first;
-        first ->link = NULL;
+        temp->link = last;
+        last ->link = temp;
         return temp;
     }
-    temp->link = first;
+    temp->link = last;
     return temp;
 }
-void search()
-{
-    int key;
-    NODE cur;
-    printf("Enter the key");
-    scanf("%d",&key);
-    cur = first;
-    while(cur!=NULL)
-    {
-        if(cur->data == key)
-        {
-            printf("Key Found");
-            return;
-        }
+NODE delete_end() {
+    NODE cur, prev;
+    if (last == NULL) {
+        printf("Linked List Empty");
+        return NULL;
+    }
+    if (last->link == last) { // Only one node in the list
+        printf("Last Node deleting %d", last->data);
+        free(last);
+        last = NULL;
+        return NULL;
+    }
+    cur = last->link;
+    while (cur->link != last) {
+        prev = cur;
         cur = cur->link;
     }
-    printf("Key not found");
+    printf("Last Node deleting %d", cur->data);
+    prev->link = last->link; 
+    printf("Last Node deleting %d",last ->data);// Update the link of the second-to-last node
+    free(last); // Free memory of the last node
+    last = prev; // Update the last pointer
+    return last;
+}
+void display()
+{
+    NODE temp;
+    if ( last == NULL ) /* Check for empty list */
+    {
+    printf("List is empty\n");
+    return;
+    }
+    printf("Contents of the list is\n"); /* Display till we get last node */
+    temp = last->link; /* Get the address of first node */
+    while (temp != last) /* Traverse till the end */
+    {
+    printf("%d ",temp->data);
+    temp = temp->link;
+    }
+    printf("%d\n", temp->data); /* Display last node */
 }
 
 void main()
+
 {
    int n, i;
    printf("Enter the no of nodes to be enterd");
    scanf("%d",&n); 
    for(i=0;i<n;i++)
    {
-    first = insert_front();
+    last = insert_front();
    }
-   search();
+   last = delete_end();
+   display();
 }
